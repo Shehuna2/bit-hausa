0f3de4e6e6c0b66509d5c4840557a3acf043fe00
Shehu Usman <79405724+jikalle@users.noreply.github.com>
2023-06-17 12:13:31 +0100

    Initial push

---
+++ bithausa/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from core.sitemaps import PostSitemap
+
+sitemaps = {
+    'posts':PostSitemap,
+}
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('core.urls', namespace='core')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps':sitemaps}, name='django.contrib.sitemaps.views.sitemap')
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
