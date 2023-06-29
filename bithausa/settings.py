a8a49393e136396a2ede8a901402913552df3bcd
Shehuna2 <135225592+Shehuna2@users.noreply.github.com>
2023-06-29 13:02:27 +0100

    Update settings.py

--- bithausa/settings.py
+++ bithausa/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images/')
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 
 # Default primary key field type
